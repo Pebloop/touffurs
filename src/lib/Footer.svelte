0ab08db8657ea4c77fb40f4d882e3320f9137dcd
jpouny <julie.pouny@clever-cloud.com>
2025-06-17 23:55:27 +0200

    Réseau sociaux intéractifs

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,3 +1,33 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	interface SocialLink {
+		id: number;
+		name: string;
+		link: string;
+		icon: string;
+	}
+
+	let socials: SocialLink[] = [];
+
+	onMount(async () => {
+		try {
+		const res = await fetch('https://app-fff52561-df06-4c55-a0e9-7abe82722bca.cleverapps.io/api/reseaux-sociauxes?populate=*');
+		if (!res.ok) throw new Error(`Erreur HTTP: ${res.status}`);
+
+		const data = await res.json();
+		socials = data.data.map((item: any) => ({
+			id: item.id,
+			name: item.name,
+			link: item.link,
+			icon: item.miniatureURL
+		}));
+		} catch (error) {
+		console.error('Erreur lors du chargement des réseaux sociaux:', error);
+		}
+	});
+</script>
+
 <!-- src/lib/Footer.svelte -->
 <footer class="bg-gray-900 text-white w-full px-6 py-12">
 	<div class="max-w-screen-xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-8">
@@ -28,23 +58,16 @@
 
 		<!-- Réseaux -->
 		<div>
-			<h2 class="text-xl font-bold mb-2">Suivez-nous</h2>
-			<ul class="space-y-1 text-sm">
-				<li class="flex items-center gap-2">
-					<img src="https://img.icons8.com/lollipop/144/twitterx.png" alt="Twitter" class="w-5 h-5 inline" />
-					<a href="https://twitter.com/Touffurs" target="_blank" class="hover:underline">Twitter</a>
-				</li>
-				<li class="flex items-center gap-2">
-					<img src="https://img.icons8.com/fluency/144/instagram-new.png" alt="Instagram" class="w-5 h-5 inline" />
-					<a href="https://www.instagram.com/" target="_blank" class="hover:underline">Instagram</a>
-				</li>
-				<li class="flex items-center gap-2">
-					<img src="https://img.icons8.com/color/144/telegram-app--v1.png" alt="Telegram" class="w-5 h-5 inline" />
-					<a href="https://t.me/tlsfurs" target="_blank" class="hover:underline">Telegram</a>
-				</li>
-			</ul>
+		<h2 class="text-xl font-bold mb-2">Suivez-nous</h2>
+		<ul class="space-y-1 text-sm">
+			{#each socials as social}
+			<li class="flex items-center gap-2">
+				<img src={social.icon} alt={social.name} class="w-5 h-5 inline" />
+				<a href={social.link} target="_blank" class="hover:underline">{social.name}</a>
+			</li>
+			{/each}
+		</ul>
 		</div>
-
 	</div>
 
 	<p class="text-center text-xs text-gray-400 mt-1">
